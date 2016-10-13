29cc6c3a128d2aacd4fe6ba6177bbc1313bd82ef
Yaroslav Putria <yaroslav.putria@avid.com>
2016-10-13 16:29:56 +0300

    copied tutorial files

---
+++ angular2 tutorial/app/hero-detail.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params }   from '@angular/router';
+import { Location }                 from '@angular/common';
+
+import { HeroService } from './hero.service';
+
+import { Hero } from './hero';
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-hero-detail',
+  templateUrl: '../templates/hero-detail.component.html',
+  styleUrls: [ '../css/hero-detail.component.css' ]
+})
+export class HeroDetailComponent implements OnInit {
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+
+  hero: Hero;
+
+  ngOnInit(): void {
+    this.route.params.forEach((params: Params) => {
+      let id = +params['id'];
+      this.heroService.getHero(id)
+        .then(hero => this.hero = hero);
+    });
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  save(): void {
+    this.heroService.update(this.hero)
+      .then(() => this.goBack());
+  }
+
+}
